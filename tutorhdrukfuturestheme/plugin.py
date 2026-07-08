07ad6d38369becb7e9e12718b2555d6bbba31761
Will <49617560+WillEVTDigital@users.noreply.github.com>
2026-07-08 20:26:45 +0100

    chore(plugin/brand-repo): Update to v3.1.0

--- tutorhdrukfuturestheme/plugin.py
+++ tutorhdrukfuturestheme/plugin.py
@@ -122,7 +122,7 @@ for mfe in hdruk_styled_mfes:
             (
                 f"mfe-dockerfile-post-npm-install-{mfe}",
                 """
-RUN npm install '@edx/brand@git+https://github.com/evtdigital-hdruk/brand-hdruk.git#feature/rebrand'
+RUN npm install '@edx/brand@git+https://github.com/evtdigital-hdruk/brand-hdruk.git#hdr-uk/v3.1.0'
 
 """,
             )
@@ -133,7 +133,7 @@ RUN npm install '@edx/brand@git+https://github.com/evtdigital-hdruk/brand-hdruk.
 hooks.Filters.ENV_PATCHES.add_item(
     (
         "mfe-dockerfile-post-npm-install-authn",
-        "RUN npm install '@edx/brand@git+https://github.com/evtdigital-hdruk/brand-hdruk.git#feature/rebrand'",
+        "RUN npm install '@edx/brand@git+https://github.com/evtdigital-hdruk/brand-hdruk.git#hdr-uk/v3.1.0'",
     )
 )
 
